f997424410787e6ec7bc1d43d0ba3a152873b1e5
jujubeyun <kre0708@naver.com>
2024-08-09 09:58:44 +0900

    create xcode project

---
+++ Photos-ish/Photos_ishApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photos_ishApp.swift
+//  Photos-ish
+//
+//  Created by Juhyun Yun on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct Photos_ishApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
